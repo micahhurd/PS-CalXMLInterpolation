9499ce6ee3a5c74c5769750d5da3781711746cb5
micahhurd <53592266+micahhurd@users.noreply.github.com>
2020-02-20 20:06:36 -0600

    Insert into XML file is now working. Still lacks column reo-ordering.

--- PS-CalXMLInterpolation.py
+++ PS-CalXMLInterpolation.py
@@ -10,38 +10,6 @@ def readXMLFile(filename):
     f.close()
     return x
 
-# print(rhoFreqList)
-# print(cfFreqListNew)
-# print(cfListNew)
-# print(uncListNew)
-
-def interpStart(x,x1,x2,y1,y2):
-    a1 = x2 - x1
-    a2 = y2 - y1
-    a3 = a2 / a1
-    a4 = x1 - x
-
-    y = y1 - (a4 * a3)
-    return y
-
-def interpEnd(x,x1,x2,y1,y2):
-    a1 = x2 - x1
-    a2 = y2 - y1
-    a3 = a2 / a1
-    a4 = x - x2
-
-    y = y2 + (a4 * a3)
-    return y
-
-def interpMid(x,x1,x2,y,y2):
-    a1 = x2 - x
-    a2 = y2 - y
-    a3 = a2 / a1
-    a4 = x1 - x
-
-    y1 = y + (a4 * a3)
-    return y1
-
 def interpolation(yVal,x,y):
 
     # print(x)
@@ -72,8 +40,116 @@ def performInterpolation():
             cfListNew[index] = newCFVal
             uncListNew[index] = newUncVal
 
+
+def insertCalFactor(index,newCFBlock):
+    print(newCFBlock)
+    print("IndexLoc {}".format(xmlDataNew[index]))
+
+
+    print(newCFBlock)
+    tempLength = len(newCFBlock)
+    tempCounter = tempLength
+    while tempCounter >= 1:
+        tempCounter -= 1
+        data = newCFBlock[tempCounter]
+
+        xmlDataNew.insert(index, data)
+
+
+
+def editCFblock(cfBlockList,frequency,calFactor,uncertainty):
+
+    newCFBlock = cfBlockList.copy()
+
+    for index, element in enumerate(newCFBlock):
+
+        filterList1 = ['<OnLabel>', '<dB>', '<DUT_Power_Avg>', '<DeviationError>', '<RFOnStdDev>', '<DUT_Power_1>',
+                       '<MisMatchFactor>']
+        filterList2 = ['</OnLabel>', '</dB>', '</DUT_Power_Avg>', '</DeviationError>', '</RFOnStdDev>',
+                       '</DUT_Power_1>', '</MisMatchFactor>']
+
+        if ("<Frequency>" in element) and ("</Frequency>" in element):
+            print(element)
+            line = element.split(">")
+            element1 = line[0] + ">"
+            line = element.split("<")
+            element2 = "<" + line[2]
+            print(element1)
+            print(element2)
+
+            if frequency > 1:
+                frequency = "{:,.0f}".format(frequency)
+            else:
+                frequency = "{:,.6f}".format(frequency)
+
+            line = "{}{}{}".format(element1,frequency,element2)
+            print(line)
+            newCFBlock[index] = line
+
+        elif ("<CalFactor>" in element) and ("</CalFactor>" in element):
+            print(element)
+            line = element.split(">")
+            element1 = line[0] + ">"
+            line = element.split("<")
+            element2 = "<" + line[2]
+            print(element1)
+            print(element2)
+
+            calFactor = "{:.4f}".format(calFactor)
+
+            line = "{}{}{}".format(element1,calFactor,element2)
+            print(line)
+            newCFBlock[index] = line
+
+        elif ("<Uncertainty>" in element) and ("</Uncertainty>" in element):
+            print(element)
+            line = element.split(">")
+            element1 = line[0] + ">"
+            line = element.split("<")
+            element2 = "<" + line[2]
+            print(element1)
+            print(element2)
+
+            uncertainty = "{:.4f}".format(uncertainty)
+
+            line = "{}{}{}".format(element1,uncertainty,element2)
+            print(line)
+            newCFBlock[index] = line
+        else:
+            for index2, i in enumerate(filterList1):
+
+                filter1 = i
+                filter2 = filterList2[index2]
+
+                if (filter1 in element) and (filter2 in element):
+                    print(element)
+                    line = element.split(">")
+                    element1 = line[0] + ">"
+                    line = element.split("<")
+                    element2 = "<" + line[2]
+                    print(element1)
+                    print(element2)
+
+                    insert = "- -"
+
+                    line = "{}{}{}".format(element1, insert, element2)
+                    # print(line)
+                    # print("--------------------------------------------")
+                    # print("filter1 {}".format(filter1))
+                    # print("filter2 {}".format(filter2))
+                    # print("element {}".format(element))
+                    # print("line {}".format(line))
+                    newCFBlock[index] = line
+                    # print("newCFBlock {}".format(newCFBlock))
+                    # input("Press Any Key To Continue...")
+
+
+
+    return newCFBlock
+
+# Start Program =================================================================
 filename = "test2.XML"
-x = readXMLFile(filename)
+xmlData = readXMLFile(filename)
 
 
 rhoFreqList = []
@@ -82,28 +158,28 @@ cfList = []
 uncList = []
 
 # Read in the existing Rho and CF data
-for index, line in enumerate(x):
+for index, line in enumerate(xmlData):
 
     if ("RhoData diffgr" in line):
         lineList = line.split(" ")
-        rFreq = x[index + 1]
+        rFreq = xmlData[index + 1]
         rFreq = re.sub("[^0-9.]", "", rFreq)
         rFreq = float(rFreq)
         rhoFreqList.append(rFreq)
 
     if ("CalFactor diffgr" in line):
         lineList = line.split(" ")
-        cFreq = x[index + 1]
+        cFreq = xmlData[index + 1]
         cFreq = re.sub("[^0-9.]", "", cFreq)
         cFreq = float(cFreq)
         cfFreqList.append(cFreq)
 
-        cf = x[index + 2]
+        cf = xmlData[index + 2]
         cf = re.sub("[^0-9.]", "", cf)
         cf = float(cf)
         cfList.append(cf)
 
-        unc = x[index + 3]
+        unc = xmlData[index + 3]
         unc = re.sub("[^0-9.]", "", unc)
         unc = float(unc)
         uncList.append(unc)
@@ -111,6 +187,7 @@ for index, line in enumerate(x):
 cfFreqListNew = []
 cfListNew = []
 uncListNew = []
+requiredInterpList = []
 for index, freq in enumerate(rhoFreqList):
 
     if (freq in cfFreqList):
@@ -122,10 +199,14 @@ for index, freq in enumerate(rhoFreqList):
 
         tempValue = uncList[tempIndex]
         uncListNew.append(tempValue)
+
+        requiredInterpList.append(0)
     else:
         cfFreqListNew.append(float(0))
         cfListNew.append(float(0))
         uncListNew.append(float(0))
+
+        requiredInterpList.append(1)
 
 
 # print(cfFreqListNew)
@@ -159,17 +240,82 @@ for index, freq in enumerate(rhoFreqList):
 
 performInterpolation()
 
-print(cfFreqListNew)
-print(cfListNew)
-print(uncListNew)
-
-print(len(cfFreqListNew))
-print(len(cfListNew))
-print(len(uncListNew))
 
 
+# Create Cal Factor Points Template by copying existing CF block into list
+tempBool = False
+cfBlockList = []
+for index, line in enumerate(xmlData):
+
+    if tempBool == True:
+        break
+
+    if ("CalFactor diffgr" in line):
+        cfBlockList.append(line)
+
+        tempCounter = 1
+        while tempBool == False:
+            line2 = xmlData[index + tempCounter]
+            cfBlockList.append(line2)
+            tempCounter += 1
+            if ("</CalFactor>" in line2) and (not "<CalFactor>" in line2):
+                tempBool = True
+
+cfBlockLength = len(cfBlockList)
+# print(cfBlockList)
+
+# print(cfFreqListNew)
+# print(cfListNew)
+# print(uncListNew)
+# print(requiredInterpList)
+#
+# print(len(cfFreqListNew))
+# print(len(cfListNew))
+# print(len(uncListNew))
+# print(len(requiredInterpList))
+
+# print(cfBlockList)
+# print(newCFblock)
 
 
+
+# Determine where an interpolated CF block needs to be entered into the existing data
+xmlDataNew = xmlData.copy()
+for index, element in enumerate(requiredInterpList):
+
+    if element == 1:
+        freqToAdd = cfFreqListNew[index]
+        CFToAdd = cfListNew[index]
+        uncertaintyToAdd = uncListNew[index]
+
+        print("freqToAdd {}".format(freqToAdd))
+
+        for index2, line in enumerate(xmlData):
+
+            if ("CalFactor diffgr" in line):
+                # Get the current frequency of the CF block
+                lineList = xmlData[index2 + 1]
+                # print(lineList)
+                freq = re.sub("[^0-9.]", "", lineList)
+                freq = float(freq)
+                #print("freq {}".format(freq))
+
+                if freq > freqToAdd:
+
+                    newCFBlock = editCFblock(cfBlockList, freqToAdd, CFToAdd, uncertaintyToAdd)
+                    tempIndex = index2
+                    print("newCFBlock {}".format(newCFBlock))
+                    print("cfBlockLength {}".format(cfBlockLength))
+
+                    print("tempIndex {}".format(tempIndex))
+
+                    insertCalFactor(tempIndex,newCFBlock)
+                    break
+
+
+with open('test0001.xml', 'w') as filehandle:
+    for listitem in xmlDataNew:
+        filehandle.write(listitem)
 
 
 
